b4f219b3074aaee89986f30f75f7ba59f2fedde6
Mulonk <j.karounis96@gmail.com>
2024-10-30 04:09:47 +0200

    Learn  promises and convert code to promises

--- javascript-amazon-project-main/Scripts/checkout.js
+++ javascript-amazon-project-main/Scripts/checkout.js
@@ -2,11 +2,25 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 //import '../data/cart-class.js';
 //import '../data/backend-practise.js'
 
-loadProducts(() => {
+Promise.all([
+    new Promise((resolve) => {
+        loadProducts(() => {
+            resolve('value1');
+        });
+    }),
+    new Promise((resolve) => {
+        loadCart(()  => {
+            resolve('value2');
+        });
+    })
+]).then((values) => {
+    console.log(values);
     renderOrderSummary();
     renderPaymentSummary();
     renderCheckoutHeader();
 });
+
